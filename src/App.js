06ef9a9f7939b0f027fe53da9bb6e59f3743680a
Florent Geron <florent.geron@orange.fr>
2022-05-31 10:51:32 +0200

    added required package

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import BottomNavbar from './components/BottomNavbar.js';
 import Navbar from './components/Navbar.js';
 import Search from  './components/Search.js';
 import AddBottle from './components/AddBottle.js';
+import Degustations from './components/Degustations.js';
+import Metrics from './components/Metrics.js';
+import MisedeCote from './components/MisedeCote.js';
 
 function App() {
   return (
@@ -15,9 +18,21 @@ function App() {
         <Route exact path="/">
           <Search />
         </Route>
+        <Route  path="/extracted">
+          <MisedeCote />
+        </Route>
+        <Route  path="/add">
+          <AddBottle />
+        </Route>
+        <Route  path="/degustations">
+          <Degustations />
+        </Route>
+        <Route  path="/metrics">
+          <Metrics />
+        </Route>
       </Switch>
+      </div>
       <BottomNavbar/>
-    </div>
     </BrowserRouter>
   );
 }
